0a319db4dfc1d378fe0caf773d809ee2d550a532
Maxim Efimov <efimov.top.academy@gmail.com>
2025-01-22 18:52:48 +0300

    22-01-25

--- TcpTranslateServerApp/Program.cs
+++ TcpTranslateServerApp/Program.cs
@@ -5,13 +5,13 @@ using System.Net.Sockets;
 using System.Text;
 
 Dictionary<string, string> words = new()
-{
-    { "table", "стол" },
-    { "color", "цвет" },
-    { "month", "месяц" },
-    { "house", "дом" },
-    { "cat", "кошка" },
-};
+    {
+        { "table", "стол" },
+        { "color", "цвет" },
+        { "month", "месяц" },
+        { "house", "дом" },
+        { "cat", "кошка" },
+    };
 
 string stopWord = "end";
 char stopChar = '\n';
@@ -28,36 +28,11 @@ try
 
     while(true) // listen and accept clients
     {
-        using TcpClient client = await server.AcceptTcpClientAsync();
+        TcpClient client = await server.AcceptTcpClientAsync();
         Console.WriteLine($"Accept client {client.Client.RemoteEndPoint}");
 
-        NetworkStream stream = client.GetStream();
-        List<byte> data = new();
-
-        int byteRead = 0;
-
-        while(true) // dialog with client
-        {
-            while((byteRead = stream.ReadByte()) != stopChar)
-            {
-                data.Add((byte)byteRead);
-            }
-
-            string word = Encoding.UTF8.GetString(data.ToArray());
-
-            if (word == stopWord)
-                break;
-
-            Console.WriteLine($"Client's word: {word}");
-            
-            string? answer = words.GetValueOrDefault(word);
-            if (answer == null) answer = "Word not found";
-
-            answer += stopChar;
-
-            await stream.WriteAsync(Encoding.UTF8.GetBytes(answer));
-            data.Clear();
-        }
+        Task.Run(async () => await ClientTask(client));
+        
     }
 }
 catch(Exception ex)
@@ -69,3 +44,40 @@ finally
     server.Stop();
 }
 
+
+async Task ClientTask(TcpClient client)
+{
+    NetworkStream stream = client.GetStream();
+    using StreamWriter writer = new StreamWriter(stream);
+    using StreamReader reader = new StreamReader(stream);
+
+    List<byte> data = new();
+
+    int byteRead = 0;
+
+    while (true) // dialog with client
+    {
+        //while ((byteRead = stream.ReadByte()) != stopChar)
+        //{
+        //    data.Add((byte)byteRead);
+        //}
+        //string word = Encoding.UTF8.GetString(data.ToArray());
+        string? word = await reader.ReadLineAsync();
+
+
+        if (word == stopWord)
+            break;
+
+        Console.WriteLine($"Client's word: {word}");
+
+        string? answer = words.GetValueOrDefault(word);
+        if (answer == null) answer = "Word not found";
+
+        answer += stopChar;
+
+        await stream.WriteAsync(Encoding.UTF8.GetBytes(answer));
+        data.Clear();
+    }
+
+    client.Close();
+}
